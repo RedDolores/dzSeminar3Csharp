f942650dc8b541f76734ffbd8258c882667e64d8
Dolores <dee12098@yandex.ru>
2022-12-07 23:20:23 +0300

    add dz

---
+++ dz19/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и 
+// проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.WriteLine("Введите число: ");
+int Num = Convert.ToInt32(Console.ReadLine());
+
+int extraNum = Num;
+int tmp = 0;
+int newNum = 0;
+
+
+while(extraNum > 0)
+{
+    tmp = extraNum % 10;
+    newNum = newNum * 10 + tmp;
+    extraNum = extraNum / 10;
+}
+
+if(newNum == Num) Console.WriteLine("yes");
+else Console.WriteLine("no");
---
+++ dz21/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и 
+// находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.WriteLine("Введите x точки A: ");
+double Ax = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите y точки A: ");
+double Ay = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите z точки A: ");
+double Az = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите x точки B: ");
+double Bx = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите y точки B: ");
+double By = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите z точки B: ");
+double Bz = Convert.ToInt32(Console.ReadLine());
+
+
+double dis = Math.Sqrt(Math.Pow(Bx - Ax, 2) + Math.Pow(By - Ay, 2) + Math.Pow(Bz - Az, 2));
+
+Console.WriteLine($"Расстояние между точками: {dis:f3}");
